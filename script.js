556110c76e0c38271f6fd007e493e446ce16b17b
ViniciusBeuther <vinicius.beuther15@gmail.com>
2023-01-05 17:12:31 -0300

    Animation

---
+++ script.js
@@ -0,0 +1,11 @@
+ const sr = ScrollReveal({
+      origin: 'top',
+      distance: '50px',
+      duration: 2000,
+      reset: true
+ });
+    
+     ScrollReveal().reveal('.banner, #off, #store-name', { delay: 200 });
+     ScrollReveal().reveal('#banner-content, #banner-content1, #title, #sub-title', { delay: 400 });
+     ScrollReveal().reveal('.image', { delay: 500 });
+  
